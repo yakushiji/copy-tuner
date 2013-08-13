d094194b275f1937b18382a58bbda3a415099c3c
yakushiji <yaoshisizhenghe@gmail.com>
2013-08-13 18:37:18 +0900

    Initial comit

---
+++ config/initializers/copy_tuner.rb
@@ -0,0 +1,4 @@
+CopyTunerClient.configure do |config|
+  config.api_key = 'a9ee1af9ee5862c4c5d3efcdf0db3d6bd64f5e8124fba565'
+  config.host = 'copy-tuner.herokuapp.com'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CopyTuner::Application.config.secret_token = '59c987bb62ef9397b6fbb5398ac069a63573a5d8a08e9434ff4f71463f7707c14d7be94549b6b11ff470cc2a76707f01110a5ef098dcbab415a9ff49709d741a'
